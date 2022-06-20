4ae9ec6ff188187b2a84d248ac024f8ef11b7c7c
Pablo Klijnjan <pablo@mana.bio>
2022-06-20 07:15:21 +0300

    change some settings for macos

--- create_links.sh
+++ create_links.sh
@@ -6,4 +6,4 @@ ln -s ~/dotfiles/gitconfig_personal ~/.gitconfig_personal
 ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
 
 ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
-ln -s ~/dotfiles/starship.toml .config/starship.toml
+ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,17 @@ end
 
 set -gx EDITOR vim
 set -gx ERL_AFLAGS "-kernel shell_history enabled"
+set -gx PIP_REQUIRE_VIRTUALENV true
+set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
+fish_add_path ~/.local/bin
 
 
 starship init fish | source
 direnv hook fish | source
-source /usr/local/opt/asdf/asdf.fish
+source /opt/homebrew/opt/asdf/asdf.fish
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/pablo/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
