759a91b651c53e56de8d826d329b9b94df182bfa
Pablo Klijnjan <pablo@mana.bio>
2023-06-27 14:26:48 +0300

    Just use the default startship.rs config

--- create_links.sh
+++ create_links.sh
@@ -6,4 +6,3 @@ ln -s ~/dotfiles/gitconfig_personal ~/.gitconfig_personal
 ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
 
 ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
-ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
