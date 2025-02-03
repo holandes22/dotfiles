3feae670bf57ee5e97e66a0cc51f19a937f6a150
Pablo Klijnjan <pablo@mana.bio>
2025-02-03 14:57:43 +0200

    Added aliases

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,13 @@ set -gx EDITOR vim
 set -gx ERL_AFLAGS "-kernel shell_history enabled"
 set -gx PIP_REQUIRE_VIRTUALENV true
 set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
+
 fish_add_path ~/.local/bin
+
+fzf --fish | source
+
+alias ff "fzf --preview 'bat --color=always {}'"
+alias uv-outdated "uv pip tree --outdated --depth 0 | grep '(latest'"
 
 
 starship init fish | source
