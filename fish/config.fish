e0fd9a65f3abb58ffb98ad4b250df619bf2ad1ab
Pablo Klijnjan <pablo@mana.bio>
2026-02-04 16:56:57 +0200

    Added alias to connect to container

--- fish/config.fish
+++ fish/config.fish
@@ -11,11 +11,31 @@ fish_add_path ~/.local/bin
 
 fzf --fish | source
 
-alias ff "fzf --preview 'bat --color=always {}'"
+# Using the following alias as fzf recommends not putting preview in FZF_DEFAULT_OPTS
+# See: https://github.com/junegunn/fzf?tab=readme-ov-file#preview-window
+alias ff "fzf --preview 'bat --color=always {}' --walker-skip=.venv,.git"
 alias uv-outdated "uv pip tree --outdated --depth 0 | grep '(latest'"
 
 
 starship init fish | source
 direnv hook fish | source
 
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
+function rabotai_ssh
+    aws ecs execute-command --profile mana-bio-rabotai --cluster rabotai-mana-bio-PROD-ecs --task $argv[1] --container rabotai-web --interactive --region eu-central-1 --command '/bin/sh'
+end
+
+
